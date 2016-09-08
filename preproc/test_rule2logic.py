3f0d2376b1e54b4601dbef905bd364d81415e158
Je-Hoon Song <song.jehoon@gmail.com>
2016-09-08 16:21:49 +0900

    Add test

---
+++ preproc/test_rule2logic.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#*************************************************************************
+# Author: {Je-Hoon Song, <song.je-hoon@kaist.ac.kr>
+#
+# This file is part of {sbie_optdrug}.
+#*************************************************************************
+
+import re 
+from sbie_optdrug.preproc import rule2logic
+from sbie_optdrug.model.published import fumia_network
+
+
+def test_this():
+
+    txtdata = re.sub('%.*\n', '\n', fumia_network.get_txtdata())
+    txtdata = re.sub('[ ;]', '', txtdata)
+    txtdata = txtdata.replace('(t)', '')
+    txtdata = txtdata.replace('(t+1)', '')
+
+    rule2logic.run(txtdata)
+
+
