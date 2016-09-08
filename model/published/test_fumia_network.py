f90398ffcf39b9384fd383595fb5be3156473d56
Je-Hoon Song <song.jehoon@gmail.com>
2016-09-08 18:55:47 +0900

    Add test

--- model/published/test_fumia_network.py
+++ model/published/test_fumia_network.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#*************************************************************************
+# Author: {Je-Hoon Song, <song.je-hoon@kaist.ac.kr>
+#
+# This file is part of {sbie_optdrug}.
+#*************************************************************************
+
 from sbie_optdrug.model.published import fumia_network
 
 
