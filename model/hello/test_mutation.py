b8c6b3b9f916f4f1092e2fd1ed059a6bf382bc88
Je-Hoon Song <song.jehoon@gmail.com>
2016-09-09 17:24:40 +0900

    Add test

---
+++ model/hello/test_mutation.py
@@ -0,0 +1,86 @@
+# The collector helper function
+#
+# Asynchronous updating rules are non deterministic and
+# need to be averaged over many runs
+#
+# The collector class makes this averaging very easy. It takes
+# a list of states and nodes to build a data structure that 
+# can compute the average the state of each node over all simulation and each timestep.
+#
+# The output of averaging (in the normalized mode) is a value between
+# 0 and 1 representing the fraction of simulation (multiply by 100 to get percent) 
+# that had the node in state True. 
+#
+from pdb import set_trace
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from sbie_optdrug import boolean2
+from sbie_optdrug.boolean2 import util
+from sbie_optdrug.util import progressbar
+import random
+
+
+# create a custom value setter
+def set_value( state, name, value, p ):
+    "Custom value setter"
+    
+    inhibitor_strength = 0.16
+    # detect the node of interest
+    if name == 'D':
+        # print 'now setting node %s' % name 
+        if value == True: 
+            if random.random() < inhibitor_strength: 
+                value = False
+
+    # this sets the attribute
+    setattr( state, name, value )
+    return value
+
+
+def test_main():
+    text = """
+    A = True
+    B = Random
+    C = Random
+    D = Random
+
+    B* = A or C
+    C* = A and not D
+    D* = B and C
+    """
+
+    repeat = 1000
+
+    coll  = util.Collector()
+    for i in range(repeat):
+        progressbar.update(i, repeat)
+        model = boolean2.Model( text, mode='async')
+        model.parser.RULE_SETVALUE = set_value
+        model.initialize()
+        model.iterate( steps=30 )
+        # in this case we take all nodes
+        # one could just list a few nodes such as [ 'A', 'B', 'C' ]
+        nodes = model.nodes
+        # this collects states for each run
+        coll.collect(states=model.states, nodes=nodes)
+
+    # this step averages the values for each node
+    # returns a dictionary keyed by nodes and a list of values
+    # with the average state for in each timestep
+    avgs = coll.get_averages( normalize=True )
+
+    # make some shortcut to data to for easier plotting
+    valueB = avgs["B"]
+    valueC = avgs["C"]
+    valueD = avgs["D"]
+
+    p1, = plt.plot( valueB , 'ob-' )
+    p2, = plt.plot( valueC , 'sr-' )
+    p3, = plt.plot( valueD , '^g-' )
+    plt.legend( [p1,p2,p3], ["B","C","D"])
+    plt.show()
+    plt.savefig('test_mutation_output.png')
+
