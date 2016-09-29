e32645a171342465fcf5334c99d38008e52acd7c
Je-Hoon Song <song.jehoon@gmail.com>
2016-09-29 10:20:37 +0900

    Start regression model

--- result/tab_s6/README.md
+++ result/tab_s6/README.md
@@ -1,1 +1,4 @@
-여기서는 LASSO, ElasticNet 같은 방법을 이용해서 CCLE, GDSC의 Therapy 데이터를 예측해 보고자 한다.
+### Table S6. Regression Model
+
+* (a)
+
---
+++ result/tab_s6/program.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#*************************************************************************
+# Author: {Name, <email>
+#
+# This file is part of {sbie_optdrug}.
+#*************************************************************************
+
+from os.path import dirname,join
+from util import progressbar
+from pdb import set_trace
+
+""" requirements """
+inputfile_a = join(dirname(__file__), 'TABLE.SXX.INPUTDATA.CSV')
+
+""" results """
+outputfile_a = join(dirname(__file__), 'TABLE.SXX.OUTPUTDATA.CSV')
+
+config = {
+    'program': 'template', 
+    'parameters': {
+        'k1': 1, 
+        'k2': 2
+        }, 
+    'input': {
+        'a': inputfile_a
+        }, 
+    'output': {
+        'a': outputfile_a
+        }
+    }
+
+
+def getconfig():
+
+    return config
+
+
+def run(config=None):
+
+    with open(config['output']['a'], 'w') as fobj:
+        fobj.write('hello')
+
