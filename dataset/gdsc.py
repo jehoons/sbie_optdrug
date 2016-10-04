e102e3391a6ba63e64c36bd6b16a26811bc5e9d7
Je-Hoon Song <song.jehoon@gmail.com>
2016-10-04 22:13:09 +0900

    Add gdsc loader

---
+++ dataset/gdsc.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#*************************************************************************
+# Author: Je-Hoon Song, <song.je-hoon@kaist.ac.kr>
+#
+# This file is part of {pyhet}.
+#*************************************************************************
+
+import pickle
+from sbie_optdrug.dataset import filelist
+import json
+import pandas as pd
+
+import ipdb
+
+rawfiles = filelist.gdsc_v50['raw']
+
+# print rawfiles.keys()
+
+df = pd.read_csv( rawfiles['gdsc_manova_output_w5'] ) 
+
+ipdb.set_trace()
