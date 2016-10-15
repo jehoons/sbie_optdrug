55e4783862c6fc459263ab3ea15db44a1ed8f8ec
Je-Hoon Song <song.jehoon@gmail.com>
2016-10-15 18:36:26 +0900

    Update contents

--- result/tab_s7/test_program.py
+++ result/tab_s7/test_program.py
@@ -12,19 +12,6 @@ from ipdb import set_trace
 from boolean3_addon import attr_cy
 import json
 
-# def test_hello(with_small, force):
-#     text='''
-#     A=Random
-#     B=Random
-#     C=Random
-#     A*= A or C
-#     B*= A and C
-#     C*= not A or B
-#     '''
-#     attr_cy.build(text)
-#     res = attr_cy.run(samples=100, steps=30, debug=False)
-#     json.dump(res, open('output.json', 'w'), indent=4)
-
 
 def check_outputs(config):
 
@@ -32,6 +19,14 @@ def check_outputs(config):
         in config['output'] ]
 
     return numpy.product(exist_list)
+
+
+def test_this(with_small, force):
+
+    default_config = program.getconfig()
+    program.run_step1(config=default_config, force=force)
+    program.run_step2(config=default_config, force=force)
+    program.run_step2_plot(config=default_config, force=force)
 
 
 # def test_this(with_small, force):
@@ -44,13 +39,3 @@ def check_outputs(config):
 #     # assert check_outputs(default_config)
 
 #     program.run(default_config)
-
-def test_this(with_small, force):
-    default_config = program.getconfig()
-    
-    program.run_step1(default_config)
-    
-    program.run_step2(default_config)
-    
-    program.run_step2_plot(default_config)
-
