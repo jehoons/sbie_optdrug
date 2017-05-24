77077ec3c105d70b57620af8cf51a56559f82cb1
ksun930508 <ksun930508@gmail.com>
2017-05-24 20:25:42 +0900

    .

--- result_season2/tab_s1/test_c.py
+++ result_season2/tab_s1/test_c.py
@@ -3,10 +3,10 @@ from os.path import exists
 from numpy.random import random
 from boolean3_addon import attr_cy, to_logic
 from pdb import set_trace
-from boolean3_addon import to_logic 
+from boolean3_addon import to_logic
 from os.path import exists,dirname
 import pandas as pd
-from tqdm import tqdm 
+from tqdm import tqdm
 
 from sbie_optdrug.result_season2 import tab_s1
 from sbie_optdrug.result_season2.tab_s1 import fumia_phenotype
@@ -16,10 +16,10 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-def get_input_combinations():    
+def get_input_combinations():
 
     inputs = [ {
-    'State_Mutagen' : State_Mutagen, 
+    'State_Mutagen' : State_Mutagen,
     'State_GFs': State_GFs,
     'State_Nutrients': State_Nutrients,
     'State_TNFalpha': State_TNFalpha,
@@ -42,7 +42,7 @@ def get_mutations_config_all():
             'State_Smad': State_Smad,
             'State_PTEN': State_PTEN,
             'State_p53': State_p53,
-            }   
+            }
 
         list_mutations_config.append(mutations_config)
 
@@ -50,62 +50,118 @@ def get_mutations_config_all():
 
 
 def get_mutations_config():
-   
+
     # free
-    mutations_config_list = [ 
+    mutations_config_list = [
         {
-            'State_APC': None,
-            'State_Ras': None,
+            'State_APC': 'off',
+            'State_Ras': 'on',
+            'State_Smad': None,
+            'State_PTEN': 'off',
+            'State_p53': None,
+        },
+
+        {
+            'State_APC': 'off',
+            'State_Ras': 'on',
             'State_Smad': None,
             'State_PTEN': None,
+            'State_p53': 'off',
+        },
+
+        {
+            'State_APC': 'off',
+            'State_Ras': None,
+            'State_Smad': 'off',
+            'State_PTEN': 'off',
             'State_p53': None,
         },
 
         {
             'State_APC': 'off',
             'State_Ras': None,
-            'State_Smad': None, 
-            'State_PTEN': None, 
-            'State_p53': None, 
-        }, 
+            'State_Smad': 'off',
+            'State_PTEN': None,
+            'State_p53': 'off',
+        },
 
         {
             'State_APC': 'off',
-            'State_Ras': 'on',
-            'State_Smad': None, 
-            'State_PTEN': None, 
-            'State_p53': None, 
-        }, 
+            'State_Ras': None,
+            'State_Smad': None,
+            'State_PTEN': 'off',
+            'State_p53': 'off',
+        },
 
         {
-            'State_APC': 'off',
+            'State_APC': None,
             'State_Ras': 'on',
             'State_Smad': 'off',
-            'State_PTEN': None, 
-            'State_p53': None, 
-        }, 
-        
+            'State_PTEN': 'off',
+            'State_p53': None,
+        },
+
         {
-            'State_APC': 'off',
+            'State_APC': None,
             'State_Ras': 'on',
             'State_Smad': 'off',
-            'State_PTEN': 'off', 
-            'State_p53': None, 
+            'State_PTEN': None,
+            'State_p53': 'off',
+        },
+
+        {
+            'State_APC': None,
+            'State_Ras': 'on',
+            'State_Smad': None,
+            'State_PTEN': 'off',
+            'State_p53': 'off',
+        },
+
+        {
+            'State_APC': None,
+            'State_Ras': None,
+            'State_Smad': 'off',
+            'State_PTEN': 'off',
+            'State_p53': 'off',
         },
 
         {
             'State_APC': 'off',
             'State_Ras': 'on',
             'State_Smad': 'off',
-            'State_PTEN': 'off', 
+            'State_PTEN': None,
+            'State_p53': 'off',
+        },
+
+        {
+            'State_APC': 'off',
+            'State_Ras': 'on',
+            'State_Smad': None,
+            'State_PTEN': 'off',
+            'State_p53': 'off',
+        },
+
+        {
+            'State_APC': 'off',
+            'State_Ras': None,
+            'State_Smad': 'off',
+            'State_PTEN': 'off',
+            'State_p53': 'off',
+        },
+
+        {
+            'State_APC': None,
+            'State_Ras': 'on',
+            'State_Smad': 'off',
+            'State_PTEN': 'off',
             'State_p53': 'off',
         }
     ]
     return mutations_config_list
 
 
-def test_c1(force):    
-    if exists(file_c1) and force==False: 
+def test_c1(force):
+    if exists(file_c1) and force==False:
         return
 
     import engine
@@ -119,48 +175,48 @@ def test_c1(force):
     repeats = 100
     ncpus = 25
 
-    list_simul_condition = [] 
+    list_simul_condition = []
     for input0 in input_combinations:
         for mut0 in mut_configs:
             simul_condition = {'input': input0, 'mutation': mut0}
-            on_states = [] 
-            off_states = [] 
+            on_states = []
+            off_states = []
 
-            for lbl in input0: 
-                if input0[lbl] == True: 
+            for lbl in input0:
+                if input0[lbl] == True:
                     on_states.append(lbl)
                 else:
                     off_states.append(lbl)
 
-            for lbl in mut0: 
-                if mut0[lbl] == 'on': 
+            for lbl in mut0:
+                if mut0[lbl] == 'on':
                     on_states.append(lbl)
                 elif mut0[lbl] == 'off':
                     off_states.append(lbl)
-                else: 
+                else:
                     pass
 
             list_simul_condition.append({
                 'simul_condition': simul_condition,
-                'on_states': on_states, 
-                'off_states': off_states, 
+                'on_states': on_states,
+                'off_states': off_states,
                 })
 
     results = []
 
-    for simul_condition in tqdm(list_simul_condition, ascii=True): 
+    for simul_condition in tqdm(list_simul_condition, ascii=True):
         on_states = simul_condition['on_states']
         off_states = simul_condition['off_states']
-        res = attr_cy.parallel(engine, 
+        res = attr_cy.parallel(engine,
                                 steps=steps,
                                 samples=samples,
                                 ncpus=ncpus,
-                                repeats=repeats, 
+                                repeats=repeats,
                                 on_states=on_states,
                                 off_states=off_states
                                 )
-        res = attach_phenotype({'input_condition': simul_condition, 
-                                'simul_result': res})        
+        res = attach_phenotype({'input_condition': simul_condition,
+                                'simul_result': res})
         results.append(res)
 
     json.dump(results, open(file_c1, 'w'), indent=4)
@@ -176,9 +232,9 @@ def test_c2_summary():
 
     df0 = pd.DataFrame([], columns=['input','mutation','A','P','Q','U'])
 
-    k = 0 
+    k = 0
 
-    for res in previous_results:        
+    for res in previous_results:
         res = attach_phenotype(res)
         inpcond = res['input_condition']['simul_condition']['input']
         inputstring = "".join(["1" if inpcond[x] else "0" for x in inplabels])
@@ -186,12 +242,12 @@ def test_c2_summary():
         mutstring = "".join(["0" if mutcond[x] == None else "1" for x in mutlabels])
         attrs = res['simul_result']['attractors']
         df0.loc[k, 'input'] = inputstring
-        df0.loc[k, 'mutation'] = mutstring    
+        df0.loc[k, 'mutation'] = mutstring
         df0.loc[k, 'A'] = 0.0
         df0.loc[k, 'P'] = 0.0
         df0.loc[k, 'Q'] = 0.0
         df0.loc[k, 'U'] = 0.0
-        for att in attrs: 
+        for att in attrs:
             df0.loc[k, attrs[att]['phenotype']] += attrs[att]['ratio']
 
         k += 1
@@ -209,7 +265,7 @@ def test_c2_summary():
 
     df2 = df0.groupby(['mutation','input']).sum()
     df3 = df2.reset_index(level=df2.index.names)
-    
+
     df4 = df3.groupby('mutation').sum()
     # (Pdb++) df4
     #                  A          P          Q        U
@@ -232,7 +288,7 @@ def test_c2_summary():
     # 11111    32.0
     # dtype: float64
 
-    for idx in df5.index.values: 
+    for idx in df5.index.values:
         df4.loc[idx] = df4.loc[idx]/df5.loc[idx]
 
     # ipdb> df4
@@ -245,57 +301,57 @@ def test_c2_summary():
     # 11110     0.599859  0.201719  0.198422  0.000000
     # 11111     0.360594  0.312500  0.326906  0.000000
 
-    plt.figure(); 
-    df4.plot(marker='o');     
+    plt.figure();
+    df4.plot(marker='o');
     plt.savefig(file_c2_plot_a + '-' + simul_mode)
+    df4.to_csv(file_c2, index = False)
+    #fumia_a = pd.read_csv('a/a4-ratio-a.csv',index_col='x')
+    #fumia_p = pd.read_csv('a/a4-ratio-p.csv',index_col='x')
+    #fumia_q = pd.read_csv('a/a4-ratio-q.csv',index_col='x')
 
-    fumia_a = pd.read_csv('a/a4-ratio-a.csv',index_col='x')
-    fumia_p = pd.read_csv('a/a4-ratio-p.csv',index_col='x')
-    fumia_q = pd.read_csv('a/a4-ratio-q.csv',index_col='x')
+    #plt.figure();
+    #line1, = plt.plot(df4['A'], fumia_a['Curve1'],'o', label='A');
+    #line2, = plt.plot(df4['P'], fumia_p['Curve1'], 'o', label='P');
+    #line3, = plt.plot(df4['Q'], fumia_q['Curve1'],'o', label='Q');
 
-    plt.figure(); 
-    line1, = plt.plot(df4['A'], fumia_a['Curve1'],'o', label='A');
-    line2, = plt.plot(df4['P'], fumia_p['Curve1'], 'o', label='P');
-    line3, = plt.plot(df4['Q'], fumia_q['Curve1'],'o', label='Q');
-
-    plt.plot([0,1],[0,1], color='0.75', alpha=0.4);
-    plt.legend(handles=[line1,line2,line3])    
-    plt.xlabel('Reproduced data')
-    plt.ylabel('Original data')    
-    plt.savefig(file_c2_plot_b + '-' + simul_mode)
-
-
+    #plt.plot([0,1],[0,1], color='0.75', alpha=0.4);
+    #plt.legend(handles=[line1,line2,line3])
+    #plt.xlabel('Reproduced data')
+    #plt.ylabel('Original data')
+    #plt.savefig(file_c2_plot_b + '-' + simul_mode)
 
 
-# input 
-file_a2 = join(dirname(tab_s1.__file__), 'a', 
+
+
+# input
+file_a2 = join(dirname(tab_s1.__file__), 'a',
                 'a2-fumia-model-processed-weighted-sum.txt')
 
-# output 
-file_c1 = join(dirname(tab_s1.__file__), 'c', 
-                'c1-simul-results.json')
+# output
+file_c1 = join(dirname(tab_s1.__file__), 'd',
+                'd1-simul-results_part2.json')
 
-file_c2 = join(dirname(tab_s1.__file__), 'c', 
-                'c2-simul-results-summary.csv')
+file_c2 = join(dirname(tab_s1.__file__), 'd',
+                'd2-simul-results-summary_part2.csv')
 
-file_c2_plot_a = join(dirname(tab_s1.__file__), 'c', 
-                    'c2-simul-results-summary')
+file_c2_plot_a = join(dirname(tab_s1.__file__), 'd',
+                    'd2-simul-results-summary_part2')
 
-file_c2_plot_b = join(dirname(tab_s1.__file__), 'c',
-                    'c2-simul-results-parity')
+#file_c2_plot_b = join(dirname(tab_s1.__file__), 'd',
+                    #'c2-simul-results-parity')
 
 
 with open(file_a2, 'r') as fobj:
     lines = fobj.readlines()
-    lines2 = [] 
-    for lin in lines: 
+    lines2 = []
+    for lin in lines:
         lin = lin.strip()
-        if lin[0] == '#': 
-            continue 
+        if lin[0] == '#':
+            continue
         right = lin.split('=')[1].strip()
-        if right == 'input':            
-            lines2.append( lin.split('=')[0].strip() + '=' + 'False') 
-        else: 
+        if right == 'input':
+            lines2.append( lin.split('=')[0].strip() + '=' + 'False')
+        else:
             lines2.append(lin)
 
     modeltext = "\n".join(lines2)
@@ -309,7 +365,7 @@ inplabels = [
     'State_GFs',
     'State_Nutrients',
     'State_TNFalpha',
-    'State_Hypoxia' 
+    'State_Hypoxia'
     ]
 
 mutlabels = [
@@ -317,7 +373,5 @@ mutlabels = [
     'State_Ras',
     'State_Smad',
     'State_PTEN',
-    'State_p53' 
+    'State_p53'
     ]
-
-
